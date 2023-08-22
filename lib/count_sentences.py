f10a0c65a21fd61e776c2d9f0c396e4d5d8f5320
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-08-22 12:33:08 +0300

    complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+  def __init__(self, value):
+       self.value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @_value.setter
+  def value(self, new_value):
+    if isinstance(new_value, str):
+      print(f"Setting value to '{new_value}'.")
+      self._value = new_value
+    else:
+      print("Value must be a string.")    
+
+  
+  def is_sentence(self):
+    return self._value.endswitch('.')
